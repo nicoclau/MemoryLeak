76bbb9aec44a8886b9189ec8ea839d3efbd1267f
nicolasbarlatier <nicolas.barlatier@misterfly.com>
2022-10-01 17:18:16 +0200

    Add project files.

---
+++ MemoryLeak/MyWorker.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MemoryLeak
+{
+    public class MyWorker : BackgroundService
+    {        
+        private readonly IMemoryCache _memoryCache;
+
+        private readonly ILogger<MyWorker> _logger;
+
+        public MyWorker(ILogger<MyWorker> logger, IMemoryCache memoryCache)
+        {
+            _logger = logger;
+            _memoryCache = memoryCache;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+                
+                var cacheEntryOptions = new MemoryCacheEntryOptions();
+                
+                _memoryCache.Set(Guid.NewGuid(), new byte[10000], cacheEntryOptions);
+            }
+        }
+    }
+}
---
+++ MemoryLeak/Program.cs
@@ -0,0 +1,13 @@
+﻿using MemoryLeak;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddMemoryCache();
+        services.AddHostedService<MyWorker>();
+    })
+    .Build();
+
+await host.RunAsync();
